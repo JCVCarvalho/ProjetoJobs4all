79c1f645d66140f525995c7298d09cb4880ced91
KRochaS <karinerocha.s@hotmail.com>
2021-09-13 22:35:46 -0300

    restapi

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND =  window.location.hostname.includes('localhost')
 ? 'http://localhost:8080'
-: 'https://restapijobs4all.herokuapp.com/';
+: 'https://restapijobs4all.herokuapp.com';
 
 
 export default {
