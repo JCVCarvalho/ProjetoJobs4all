c5e374fda682c2d76b17ccf333f5bc702bfbba34
JCVCarvalho <jcvargasdecarvalho@gmail.com>
2021-09-21 19:19:32 -0300

    Fix URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND =  window.location.hostname.includes('localhost')
 ? 'http://localhost:8080'
-: 'https://apijobs4all.herokuapp.com/';
+: 'https://apijobs4all.herokuapp.com';
 
 
 export default {
