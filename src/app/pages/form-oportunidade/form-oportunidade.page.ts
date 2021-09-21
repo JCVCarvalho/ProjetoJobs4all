bea2da065e551145f7ece18ac60433729104d23c
JCVCarvalho <jcvargasdecarvalho@gmail.com>
2021-09-21 19:09:48 -0300

    commits

--- src/app/pages/form-oportunidade/form-oportunidade.page.ts
+++ src/app/pages/form-oportunidade/form-oportunidade.page.ts
@@ -5,62 +5,60 @@ import { Jobs4allService } from 'src/app/jobs4all.service';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
-  selector: 'app-form-oportunidade',
-  templateUrl: './form-oportunidade.page.html',
-  styleUrls: ['./form-oportunidade.page.scss'],
+	selector: 'app-form-oportunidade',
+	templateUrl: './form-oportunidade.page.html',
+	styleUrls: ['./form-oportunidade.page.scss'],
 })
 export class FormOportunidadePage implements OnInit {
 
-    public formOportunidade = this.formBuilder.group({
-        nome: [null, Validators.required],
-        whatsapp: [null, Validators.required],
-        crnm: [null, Validators.required],
-        dataNascimento: [null, Validators.required],
-        endereco: [null, Validators.required],
-        formacaoAcademica: [null, Validators.required],
-        habilidades: [0],
-        descricao: [null, Validators.required],
-        tempoExperiencia: [null, Validators.required],
-      });
+	public formOportunidade = this.formBuilder.group({
+		nome: [null, Validators.required],
+		whatsapp: [null, Validators.required],
+		crnm: [null, Validators.required],
+		dataNascimento: [null, Validators.required],
+		endereco: [null, Validators.required],
+		formacaoAcademica: [null, Validators.required],
+		habilidades: [0],
+		descricao: [null, Validators.required],
+		tempoExperiencia: [null, Validators.required],
+	});
 
-      public location;
+	public location;
 
-  constructor(
-      private formBuilder: FormBuilder,
-      private _jobs4allService: Jobs4allService,
-      private toastCtrl: ToastController,
-      private geolocation: Geolocation
-      ) { }
+	constructor(
+		private formBuilder: FormBuilder,
+		private _jobs4allService: Jobs4allService,
+		private toastCtrl: ToastController,
+		private geolocation: Geolocation
+	) { }
 
-  ngOnInit() {
-  }
-
-
-  requestSaveEmpregador() {
+	ngOnInit() {
+	}
 
 
-    this._jobs4allService.saveColaborador(this.formOportunidade.value).subscribe((colaborador) => {
-        console.log(colaborador);
-        alert('Seu cadastro foi concluído com sucesso');
-    });
-}
+	requestSaveEmpregador() {
 
-showMap() {
 
-    this.geolocation.getCurrentPosition().then((resp) => {
-       }).catch((error) => {
-         console.log('Error getting location', error);
-       });
-       
-       let watch = this.geolocation.watchPosition();
-       watch.subscribe((data: any) => {
-        window.open(`https://www.google.com/maps/search/?api=1&query=${data.coords.latitude},${data.coords.longitude}`, '_blank');
-       });
-    
-  
-    }
+		this._jobs4allService.saveColaborador(this.formOportunidade.value).subscribe((colaborador) => {
+			console.log(colaborador);
+			alert('Seu cadastro foi concluído com sucesso');
+		});
+	}
 
-  
+	showMap() {
+
+		this.geolocation.getCurrentPosition().then((resp) => {
+			window.open(`https://www.google.com/maps/search/?api=1&query=${resp.coords.latitude},${resp.coords.longitude}`, '_blank');
+
+		}).catch((error) => {
+			console.log('Error getting location', error);
+		});
+
+
+
+	}
+
+
 }
 
 
